25ae5e4f3adcd6cf39db43cfe54b50beed5c3227
Bmercado17 <askotala@yahoo.com>
2021-08-03 09:51:16 -0700

    'funcionalidad'

--- app.js
+++ app.js
@@ -9,31 +9,59 @@ let app = Vue.createApp({
       monsterHealth: 100,
       playerOneHealth: 100,
       roundJugado: 0,
+      winner: null,
     };
   },
   computed: {
     monsterBar() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBar() {
+      if (this.playerOneHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerOneHealth + "%" };
     },
+
     // by attaching this computed method to html where triple attack currently is
     // we can get the same outcome as triple attach
     // especialAttack() {
     //   this.roundJugado % 3 !== 0;
     // },
   },
+  watch: {
+    playerOneHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        //draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // player loose
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerOneHealth <= 0) {
+        //draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        //monster lost
+        this.winner = "playerOne";
+      }
+    },
+  },
 
   methods: {
     attackMonster() {
       this.roundJugado++;
-      let attackVal = getRandomAttackVal(9, 11);
+      let attackVal = getRandomAttackVal(10, 11);
       this.monsterHealth -= attackVal;
       this.attackPlayer();
     },
     attackPlayer() {
-      let attackVal = Math.floor(Math.random() * (12 - 8)) + 8;
+      let attackVal = Math.floor(Math.random() * (7 - 18)) + 18;
       this.playerOneHealth -= attackVal;
     },
     tripleAttack() {
@@ -42,13 +70,13 @@ let app = Vue.createApp({
       this.monsterHealth -= attackVal;
       this.attackPlayer();
     },
-    healJugador() {
+    healPlayer() {
       this.roundJugado++;
-      let healValor = Math.floor(Math.random() * (12 - 9)) + 9;
-      if ((this.playerOneHealth += healValor > 100)) {
+      let curarHeal = Math.floor(Math.random() * (13 - 9)) + 9;
+      if (this.playerOneHealth + curarHeal > 100) {
         this.playerOneHealth = 100;
       } else {
-        this.playerOneHealth += healValor;
+        this.playerOneHealth += curarHeal;
       }
       this.attackPlayer();
     },
